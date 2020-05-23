06053620621d7465e481560433babdf95b71e801
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2020-05-23 09:23:01 +0200

    init

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: 'node-slack-app',
+      exec_mode: 'cluster_mode',
+      instances: '2',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '300M',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      host: 'tatyom',
+      ref: 'origin/master',
+      repo: 'git@github.com:cstuncsik/node-slack-app.git',
+      path: '/node-slack-app',
+      'pre-deploy': 'echo "Deploying to production"',
+      'post-deploy': 'pm2 startOrReload ecosystem.config.js --env production'
+    }
+  }
+}
