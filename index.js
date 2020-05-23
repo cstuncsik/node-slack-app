b75f314b5f7bdd1c2ac7941ae2c7a001744c4349
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2020-05-23 13:09:16 +0200

    listen to app home

--- index.js
+++ index.js
@@ -7,6 +7,61 @@ dotenv.config()
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
+})
+
+app.event('app_home_opened', async ({ event, context }) => {
+  try {
+    /* view.publish is the method that your app uses to push a view to the Home tab */
+    const result = await app.client.views.publish({
+
+      /* retrieves your xoxb token from context */
+      token: context.botToken,
+
+      /* the user that opened your app's app home */
+      user_id: event.user,
+
+      /* the view payload that appears in the app home */
+      view: {
+        type: 'home',
+        callback_id: 'home_view',
+
+        /* body of the view */
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            type: 'divider'
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            type: 'actions',
+            elements: [
+              {
+                type: 'button',
+                text: {
+                  type: 'plain_text',
+                  text: 'Click me!'
+                }
+              }
+            ]
+          }
+        ]
+      }
+    })
+  } catch (error) {
+    console.error(error)
+  }
 });
 
 (async () => {
