2d6dce04f0e3c0a4a0c79dfdfed9bd2ee7ab7073
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2020-05-23 13:26:46 +0200

    adding more event listeners

--- index.js
+++ index.js
@@ -62,6 +62,73 @@ app.event('app_home_opened', async ({ event, context }) => {
   } catch (error) {
     console.error(error)
   }
+})
+
+app.command('/hello', async ({ ack, payload, context }) => {
+  // Acknowledge the command request
+  await ack()
+
+  try {
+    const result = await app.client.chat.postMessage({
+      token: context.botToken,
+      // Channel to send message to
+      channel: payload.channel_id,
+      // Include a button in the message (or whatever blocks you want!)
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'Go ahead. Click it.'
+          },
+          accessory: {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'Click me!'
+            },
+            action_id: 'button_abc'
+          }
+        }
+      ],
+      // Text in the notification
+      text: 'Message from Test App'
+    })
+    console.log(result)
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+// Listen for a button invocation with action_id `button_abc`
+// You must set up a Request URL under Interactive Components on your app configuration page
+app.action('button_abc', async ({ ack, body, context }) => {
+  // Acknowledge the button request
+  ack()
+
+  try {
+    // Update the message
+    const result = await app.client.chat.update({
+      token: context.botToken,
+      // ts of message to update
+      ts: body.message.ts,
+      // Channel of message
+      channel: body.channel.id,
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: '*The button was clicked!*'
+          }
+        }
+      ],
+      text: 'Message from Test App'
+    })
+    console.log(result)
+  } catch (error) {
+    console.error(error)
+  }
 });
 
 (async () => {
